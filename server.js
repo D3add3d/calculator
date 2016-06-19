1bf193007b210a20a3efe3de8089bb628ea2fe45
pyriter <pvvuong@ucdavis.edu>
2016-06-18 21:51:55 -0700

    Fixed server port

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var server = express();
 server.use('/', express.static('dist'));
+server.set('port', (process.env.PORT || 5000))
 
-server.listen(80, function(port) {
-  console.log("Express static server started. Listening at port 80");
+server.listen(server.get('port'), function(port) {
+  console.log("Express static server started. Listening on port ", server.get('port'));
 });
 /*
 if(process.env.NODE_ENV === 'production') {
