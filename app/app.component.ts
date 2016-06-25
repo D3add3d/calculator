f50604cdd1da9d408f6a49e7770b7e04a686c4c0
pyriter <pvvuong@ucdavis.edu>
2016-06-24 22:35:18 -0700

    Added buttons and some initial styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,66 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'calculator',
+  styles: [`
+    .ca-border {
+      border-style: groove;
+    }
+    .ca-font {
+
+    }
+    #ca-number-display {
+      width: 100%;
+    }
+    button {
+      width: 25%;
+      font-size: large;
+    }
+    button.operator {
+      background-color: orange;
+    }
+    .ca-flex-container {
+      display: flex;
+      flex-direction: row;
+      height: 16.6666667%;
+
+    }
+  `],
   template: `
-    <h1>Hello World</h1>
+  <div>
+    <div class="ca-flex-container">
+      <input type="text" id="ca-number-display" readonly/>
+    </div>
+    <div class="ca-flex-container">
+      <button>AC</button>
+      <button>+/-</button>
+      <button>%</button>
+      <button class="operator">/</button>
+    </div>
+    <div class="ca-flex-container">
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button class="operator">*</button>
+    </div>
+    <div class="ca-flex-container">
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button class="operator">-</button>
+    </div>
+    <div class="ca-flex-container">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button class="operator">+</button>
+    </div>
+    <div class="ca-flex-container">
+      <button>i</button>
+      <button>0</button>
+      <button>.</button>
+      <button class="operator">=</button>
+    </div>
+  </div>
   `
 })
 export class AppComponent {
