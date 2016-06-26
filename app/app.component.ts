7c3811f2aa7e56643f065e796a5eb3730d75b3a7
pyriter <pvvuong@ucdavis.edu>
2016-06-25 19:26:12 -0700

    Added mouse click events to buttons

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,5 +7,35 @@ import '../styles.css';
   template: require('./app.component.html')
 })
 export class AppComponent {
-  
+  displayText:string = '';
+  BUTTONS = BUTTONS;
+
+  onClick(event:any, button:BUTTONS) {
+    this.displayText = event.target.innerText;
+    event.target.setAttribute("style", "opacity:1");
+  }
+
+}
+
+enum BUTTONS {
+  AC,
+  NEGATE,
+  PERCENT,
+  DIVIDE,
+  SEVEN,
+  EIGHT,
+  NINE,
+  MULTIPLY,
+  FOUR,
+  FIVE,
+  SIX,
+  SUBTRACT,
+  ONE,
+  TWO,
+  THREE,
+  ADDITION,
+  SETTINGS,
+  ZERO,
+  PERIOD,
+  EQUAL
 }
